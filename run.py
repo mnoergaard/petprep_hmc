17ea8f9fc1b20bae510ea63f6ea325a2102785aa
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-04-26 15:00:12 -0400

    update parser args to match README.md

--- run.py
+++ run.py
@@ -404,13 +404,13 @@ def plot_motion_outputs(in_file):
 
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser(description='BIDS App for PETPrep head motion correction workflow')
-    parser.add_argument('bids_dir', help='The directory with the input dataset '
+    parser.add_argument('--bids_dir', required=True,  help='The directory with the input dataset '
                     'formatted according to the BIDS standard.')
-    parser.add_argument('output_dir', help='The directory where the output files '
+    parser.add_argument('--output_dir', required=True, help='The directory where the output files '
                     'should be stored. If you are running group level analysis '
                     'this folder should be prepopulated with the results of the'
                     'participant level analysis.')
-    parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+    parser.add_argument('--analysis_level', default='participant', help='Level of the analysis that will be performed. '
                     'Multiple participant level analyses can be run independently '
                     '(in parallel) using the same output_dir.',
                     choices=['participant', 'group'])
